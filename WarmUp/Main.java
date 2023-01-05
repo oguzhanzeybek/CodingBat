553acc6e93fb61c90691c6ffbfdda76cd0919580
oguzhanzeybek <darkstroge@gmail.com>
2023-01-05 21:08:48 +0300

    Monkey

--- WarmUp/Main.java
+++ WarmUp/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-KelimeSacmalamasi sacma = new KelimeSacmalamasi();
-
-System.out.println(sacma.kelime("sanane", 2));
+        Maymun maymun = new Maymun();
+        
+        System.out.print(maymun.MaymunlarAglamasin(false, false));
     }
 }
---
+++ WarmUp/Maymun.java
@@ -0,0 +1,9 @@
+public class Maymun {
+    public boolean MaymunlarAglamasin(boolean a,boolean b){
+        if(a == b){
+            System.out.println("maymunlar agliyo ya da guluyo");
+            
+        }
+        return false;
+    }
+}
