143cfe69c5d9cedc5103141b237dbe611bcfde63
oguzhanzeybek <darkstroge@gmail.com>
2023-01-05 21:16:31 +0300

    Parrot

--- WarmUp/Maymun.java
+++
@@ -1,9 +0,0 @@
-public class Maymun {
-    public boolean MaymunlarAglamasin(boolean a,boolean b){
-        if(a == b){
-            System.out.println("maymunlar agliyo ya da guluyo");
-            
-        }
-        return false;
-    }
-}
---
+++ WarmUp/PapaganKonusursa.java
@@ -0,0 +1,8 @@
+public class PapaganKonusursa {
+    public boolean papaganKonusursa(boolean konusurmu,int saatkac){
+        
+        if((20<saatkac && saatkac<7) || konusurmu){
+            return false;
+        }return true;
+    }
+}
